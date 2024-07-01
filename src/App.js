a7686e449fb0ca13127bed4fdbda415d75a40fac
Robert Doerfler <rodo@bloerp.de>
2024-07-01 21:15:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
+import Remaining from './components/Remaining';
 
+import ExpenseTotal from './components/ExpenseTotal';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -16,7 +18,6 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
                             // Budget component
                             <div className='col-sm'>
                                 <Budget />
@@ -25,12 +26,19 @@ const App = () => {
 
                         {
                             /* Add Remaining component here*/
+                            //Remaining component
+                            
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
                         }        
 
                         {
                             /* Add ExpenseTotal component here */
                         }        
-                       
+                            <div className='col-sm'>
+                                <ExpenseTotal />
+                            </div>
                         {
                             /* Add ExpenseList component here */
                         }         
