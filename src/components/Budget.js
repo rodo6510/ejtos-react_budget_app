2c3e0890d3bd60744c799ff297d87dcb2b52fe7f
rodo6510 <rodo@bloerp.de>
2024-07-02 02:23:13 -0400

    done again

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,14 +1,18 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+import ExpenseTotal from './ExpenseTotal';
+// import { ExpenseTotal } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
-    //const { currentExp } = useContext(AppContext);
-    
     const [newBudget, setNewBudget] = useState(budget);
+    const [totalExpenses] = useState(ExpenseTotal);
+
     const handleBudgetChange = (event) => {
             if (event.target.value >20000){
                 alert("Budget cannot increase more than 20.000!");
+            }if (event.target.value < totalExpenses){
+                alert("You cannot reduce budget value lower than he spending!");    
             }else{
                setNewBudget(event.target.value);   
             }
